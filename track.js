8797f1302dfa268901615427c74356d0100297c0
Almas Yoko_office <Almas almas@yoko.space>
2025-02-25 16:15:44 +0500

    add initial implementation of Track and Ball classes; update index.html to include scripts and set up canvas

---
+++ track.js
@@ -0,0 +1,20 @@
+class Track {
+    constructor(center, radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    getPosition(offset) {
+        return {
+            x: this.center.x + Math.cos(offset) * this.radius,
+            y: this.center.y - Math.sin(offset) * this.radius
+        }
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
+        ctx.strokeStyle = 'white';
+        ctx.stroke();
+    }
+}
